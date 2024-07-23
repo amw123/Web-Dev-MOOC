c4ba7f8af04bf330d61aea61fd9adbdd3db925f9
AMW <amw@example.com>
2024-07-23 15:47:35 +0100

    add js file, cleaning up typos

---
+++ script.js
@@ -0,0 +1,14 @@
+function sayHello() {
+
+    const name =  prompt("What is your name?");
+    alert("Hello "+name+ "! Welcome to my website.");
+}
+
+sayHello();
+
+function rollDice() {
+    let dice = Math.floor(Math.random() * 6) + 1;
+    alert("You have rolled a "+ dice)
+}
+
+rollDice()
