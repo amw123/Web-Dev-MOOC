12e7b2a66a2674f27c1ecb1b695606192c01b4ec
AMW <amw@example.com>
2024-07-25 00:27:13 +0100

    Dark mode, updates section and color scheme shift

--- script.js
+++ script.js
@@ -12,3 +12,43 @@ function rollDice() {
 }
 
 // rollDice()
+
+let darkModeOn = false 
+
+function darkMode() {
+    // if (dark == true){     //  Light mode
+    //     dark = false;
+
+    //     document.body.style.color = "black";
+    //     document.h1.style.backgroundColor = "white";
+    //     document.span.style.color = "white";
+    //     document.getElementById("pos").h2.style.backgroundColor = "white";
+    //     document.getElementById("pos").h2.style.color = "black";
+    //     document.getElementById("pos-text").style.color = "black";
+    //     document.getElementById("rules-list").h2.style.backgroundColor = "white";
+    //     document.getElementById("rules-list").style.color = "black";
+    // }
+    
+    if (darkModeOn==false) {                  // Dark mode
+        document.body.style.background = "black";
+
+        document.getElementById("pos").style.background = "black";
+        document.getElementById("pos").style.color = "lightgreen";
+
+        document.getElementById("goalie").style.background = "black";
+        document.getElementById("goalie").style.color = "pink";
+
+        darkModeOn =true
+    }
+    //document.body.style.color = "white";
+    // document.h1.style.backgroundColor = "black";
+    // document.span.style.color = "light-green";
+    // document.getElementById("pos").h2.style.backgroundColor = "red";
+    // document.getElementById("pos").h2.style.color = "purple";
+    // document.getElementById("pos-text").style.color = "green";
+    // document.getElementById("rules-list").h2.style.backgroundColor = "black";
+    // document.getElementById("rules-list").style.color = "pink";
+    
+    
+} 
+
